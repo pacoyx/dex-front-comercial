1aeaa3aee0c6e319ad8f5ad6e0243101eb46e2ee
carlos bazan <pacoyx@gmail.com>
2024-12-13 15:22:24 -0500

    se agrego archivos para levantar con docker nginx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dex-front-comercial';
+
+
+  constructor() {
+    console.log('environment.apiUrlBase', environment.apiUrlBase);
+  }
+
 }
+// https://www.arunyadav.in/codehacks/blogs/post/19/create-an-angular-app-and-deploy-using-in-nginx-and-docker
